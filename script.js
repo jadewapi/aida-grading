429d571c479fcd4b296d05641457b9f29f3e5c63
jadewapi <125937514+jadewapi@users.noreply.github.com>
2023-03-06 11:32:28 -0600

    added javascript for animation testing

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const main = document.querySelector('main');
+const button = document.querySelector('.ass');
+
+
+button.addEventListener('click', function(){
+    main.classList.add('animate');
+});
+
